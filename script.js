a1226e43915c55912ecb13434b9de996265fcede
prescilla-estrade <prescilla.estrade@etu.iut-tlse3.fr>
2023-12-08 03:44:14 +0100

    Ajout mode sombre

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const stylesArray = [
-    'body { background-color: #f0f0f0; color: #333; }', // Style par défaut (minimalisme épuré)
-    'body { background-color: #ffd700; color: #8a2be2; }', // Arc-en-ciel en couleur pastel
-    'body { background-color: #ff6347; color: #fff; }', // Tomate avec texte blanc
-    'body { background-color: #00ffff; color: #000; }', // Cyan avec texte noir
-    'body { background-color: #ffa07a; color: #2f4f4f; }', // Saumon clair avec texte gris ardoise
-    'body { background: linear-gradient(to right, #ffcc00, #ff3300); color: #fff; }', // Dégradé orangé
-    'body { background: linear-gradient(to right, #663399, #ff00ff, #66ff66); color: #fff; }', // Dégradé multicolore
-    'body { background-color: #6495ed; color: #fff; }', // Bleu acier avec texte blanc
-    'body { background-color: #ff1493; color: #fff; }', // Rose vif avec texte blanc
-    'body { background-color: #7fff00; color: #000; }', // Vert chartreuse avec texte noir
-    'body { background: linear-gradient(to right, #800080, #008080); color: #fff; }', // Dégradé violet-bleu avec texte blanc
-    'body { background: linear-gradient(to right, #ff7f50, #40e0d0); color: #fff; }', // Dégradé orange-bleu turquoise avec texte blanc
-    'body { background-color: #ff8c00; color: #fff; }', // Orange foncé avec texte blanc
-    'body { background-color: #dda0dd; color: #000; }', // Violet pâle avec texte noir
-    'body { background-color: #aqua; color: #000; }', // Aqua avec texte noir
-    'body { background: linear-gradient(to right, #ffccff, #ff99cc); color: #fff; }', // Dégradé rose-violet avec texte blanc
-    'body { background: linear-gradient(to right, #00ffcc, #ffcc00); color: #000; }', // Dégradé turquoise-orange avec texte noir
+    'body { background-color: #f0f0f0; color: #333; }', 
+    'body { background-color: #ffd700; color: #8a2be2; }', 
+    'body { background-color: #ff6347; color: #fff; }', 
+    'body { background-color: #00ffff; color: #000; }', 
+    'body { background-color: #ffa07a; color: #2f4f4f; }', 
+    'body { background: linear-gradient(to right, #ffcc00, #ff3300); color: #fff; }', 
+    'body { background: linear-gradient(to right, #663399, #ff00ff, #66ff66); color: #fff; }', 
+    'body { background-color: #6495ed; color: #fff; }', 
+    'body { background-color: #ff1493; color: #fff; }', 
+    'body { background-color: #7fff00; color: #000; }', 
+    'body { background: linear-gradient(to right, #800080, #008080); color: #fff; }', 
+    'body { background: linear-gradient(to right, #ff7f50, #40e0d0); color: #fff; }', 
+    'body { background-color: #ff8c00; color: #fff; }', 
+    'body { background-color: #dda0dd; color: #000; }', 
+    'body { background-color: #aqua; color: #000; }', 
+    'body { background: linear-gradient(to right, #ffccff, #ff99cc); color: #fff; }', 
+    'body { background: linear-gradient(to right, #00ffcc, #ffcc00); color: #000; }', 
   ];
+  
+  let isDarkMode = false;
   
   function applyRandomStyle() {
     const randomIndex = Math.floor(Math.random() * stylesArray.length);
@@ -25,5 +27,20 @@ const stylesArray = [
     dynamicStyles.innerHTML = randomStyle;
   }
   
+  function toggleDarkMode() {
+    const body = document.body;
+  
+    if (!isDarkMode) {
+      // Mode sombre
+      body.style.backgroundColor = '#000';
+      body.style.color = '#fff';
+      isDarkMode = true;
+    } else {
+      // Mode aléatoire
+      applyRandomStyle();
+      isDarkMode = false;
+    }
+  }
+   
   window.onload = applyRandomStyle;
   
